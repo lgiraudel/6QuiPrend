afbd97a18cdb4c9be6d26bdb15946046e7a8d6e7
Hugo Giraudel <hugo.giraudel@gmail.com>
2013-11-24 23:00:46 +0100

    Update Player.js

--- js/Player.js
+++ js/Player.js
@@ -1,70 +1,71 @@
-/*global jQuery, window, dust, console */
+/* global jQuery, window, dust, console */
 
 (function ($) {
-	'use strict';
+    'use strict';
 
-	var SixQuiPrend = window.SixQuiPrend = window.SixQuiPrend || {};
+    var SixQuiPrend = window.SixQuiPrend = window.SixQuiPrend || {};
 
-	SixQuiPrend.Player = function (conf) {
-		this.conf = $.extend({
-			cards:   null,
-			index:   null,
-      type:    'IA',
-      display: true
-		}, conf || {});
+    SixQuiPrend.Player = function (conf) {
+        this.conf = $.extend({
+            cards:   null,
+            index:   null,
+            type:    'IA',
+            display: true
+        }, conf || {});
 
-		this.init();
-	};
-
-	SixQuiPrend.Player.prototype.init = function () {
-    var i,
-      output;
-
-		this.score = 0;
-    this.cards = [];
-
-    this.templates = {
-      player: 'player',
-      cards:  'cards',
-      card:   'card'
+        this.init();
     };
 
-    for (i = 0; i < this.conf.cards.length; i += 1) {
-      this.cards.push({
-        value: this.conf.cards[i],
-        weight: this.conf.deck.getCardValue(this.conf.cards[i])
-      });
-    }
+    SixQuiPrend.Player.prototype.init = function () {
+        var i,
+            output;
 
-    if (this.conf.display) {
-      output = dust.render(this.templates.player, {
-        name: 'User #' + this.conf.index,
-        type: this.conf.type,
-        cards: this.cards
-      }, function (err, out) {
-        $('#players').append(out);
-      });
-    }
-	};
+        this.score = 0;
+        this.cards = [];
 
-	SixQuiPrend.Player.prototype.getCard = function () {
-		var cardIndex, card;
+        this.templates = {
+            player: 'player',
+            cards:  'cards',
+            card:   'card'
+        };
 
-		cardIndex = Math.floor(Math.random() * this.conf.cards.length);
-		card = this.conf.cards.splice(cardIndex, 1);
+        for (i = 0; i < this.conf.cards.length; i += 1) {
+            this.cards.push({
+                value: this.conf.cards[i],
+                weight: this.conf.deck.getCardValue(this.conf.cards[i])
+            });
+        }
 
-		return card[0];
-	};
+        if (this.conf.display) {
+            output = dust.render(this.templates.player, {
+                name: 'User #' + this.conf.index,
+                type: this.conf.type,
+                cards: this.cards
+            }, function (err, out) {
+                $('#players').append(out);
+            });
+        }
+    };
 
-	SixQuiPrend.Player.prototype.addPoints = function (cards) {
-		var points = this.conf.deck.getCardsValue(cards);
+    SixQuiPrend.Player.prototype.getCard = function () {
+        var cardIndex, card;
 
-		console.log('Player #' + this.conf.index + ' add ' + points + ' points');
+        cardIndex = Math.floor(Math.random() * this.conf.cards.length);
+        card = this.conf.cards.splice(cardIndex, 1);
 
-		this.score += cards.length;
-	};
+        return card[0];
+    };
 
-	SixQuiPrend.Player.prototype.getScore = function () {
-		return this.score;
-	};
+    SixQuiPrend.Player.prototype.addPoints = function (cards) {
+        var points = this.conf.deck.getCardsValue(cards);
+
+        console.log('Player #' + this.conf.index + ' add ' + points + ' points');
+
+        this.score += cards.length;
+    };
+
+    SixQuiPrend.Player.prototype.getScore = function () {
+        return this.score;
+    };
+    
 }(jQuery));
